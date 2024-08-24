9c45d698295f925e878ce5a0f0f29cbe36f5ad26
guilhermeguedes <guilhermeguedes.gag@gmail.com>
2024-08-24 08:37:42 -0300

    primeiro commit

---
+++ src/main/java/com/digihub/gerenciaclientes/GerenciaClientesApplication.java
@@ -0,0 +1,13 @@
+package com.digihub.gerenciaclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digihub/gerenciaclientes/GerenciaClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digihub.gerenciaclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
